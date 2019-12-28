0b8dc578ff880a3a20ae895245b6a8f30f5beabb
Taruchit Goyal <taruchit.goyal@gmail.com>
2019-12-28 23:08:25 +0530

    Read a List in HTML

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,30 @@
 #import Flask class from flask
-from flask import Flask 
+from flask import Flask, render_template 
 #app variable is created and setting it to instance of Flask class
 # __name__ is a special variable name in Python used to name the module
 #Thus, it helps to instantiate Flask in app variable.
+#We import render_template function for routing.
 app = Flask(__name__)
+
+
+#Adding static content
+
+posts=[
+	{
+		'author':'RD Sharma',
+		'subject':'Maths',
+		'class':'All'
+	},
+	{
+		'author':'HC Verma',
+		'subject':'Physics',
+		'class':'12th'
+	}
+	
+]
+
+
+
 
 #routes are content we type in URL of browzer for going to different pages.
 #@app.route decorator helps to navigate and show correct content on UI.
@@ -13,12 +34,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1> Home Page </h1>"
+	return render_template('home.html',postList=posts)
 
 
 @app.route("/about")
 def about():
-	return "<h1> About Page </h1>"
+	return render_template('about.html')
 
 	
 #This condition is true only if script is run directly.
